92b185716fd15942c26d424cd228633c6fc48927
Carlos Jimenez <cjimenez@totalcoin.com>
2024-04-12 22:31:15 -0300

    install and config eslint, prettire, husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+ },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
