d5ee7aeca8510f777d484a7a13f13e1665e80034
Carlos Jimenez <cjimenez@totalcoin.com>
2024-04-22 21:32:26 -0300

    Change endOfLine

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,7 +9,7 @@ module.exports = {
   bracketSpacing: true,
   arrowParens: 'always',
   jsxBracketSameLine: false,
-  endOfLine: 'lf',
+  endOfLine: 'crlf',
   tailwindConfig: './tailwind.config.js',
   overrides: [
     {
